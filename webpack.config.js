c645e59c9c7ee240739a3822fe04540360d4218b
Aashis Kishore <aashiskishore@gmail.com>
2018-12-03 18:56:54 +0530

    Create configuration to load css files

--- webpack.config.js
+++ webpack.config.js
@@ -21,6 +21,7 @@ const commonConfig = merge([
             }),
         ],
     },
+    parts.loadCSS(),
 ]);
 
 const productionConfig = merge([]);
--- webpack.parts.js
+++ webpack.parts.js
@@ -1,1 +1,12 @@
-
+exports.loadCSS = ({ include, exclude } = {}) => ({
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                include,
+                exclude,
+                use: ['style-loader', 'css-loader'],
+            },
+        ],
+    },
+});
