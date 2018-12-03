cde0e75ef931c241a5957b5433bfc87ace053ad0
Aashis Kishore <aashiskishore@gmail.com>
2018-12-03 15:42:43 +0530

    Create shortcuts of git commands

---
+++ shortcuts.sh
@@ -0,0 +1,21 @@
+alias gs="git status"
+
+alias glg="git log --pretty=oneline"
+
+alias ga="git add "
+
+alias gaa="git add ."
+
+alias gc="git commit -m"
+
+alias gac="git commit -a -m"
+
+alias gp="git push -u origin master"
+
+alias gpb="git push -u origin "
+
+alias gbl="git branch -v"
+
+alias grl="git remote -v"
+
+alias gra="git remote add "
